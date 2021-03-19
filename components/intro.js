a6a675410aea5fe3275ca3237b8d5b7cdd03019a
Gabriel Luiz <gabeluiz10@gmail.com>
2021-03-18 22:50:53 -0300

    REDME update

--- components/intro.js
+++ components/intro.js
@@ -7,21 +7,7 @@ export default function Intro() {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and{' '}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
+        Talvez criar um busca aqui.
       </h4>
     </section>
   )
