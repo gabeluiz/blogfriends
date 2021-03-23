6dbfefe68b3e3571349f297dfa27aa8439646f07
Gabriel Luiz <gabeluiz10@gmail.com>
2021-03-23 18:08:54 -0300

    update layout

--- components/cover-image.js
+++ components/cover-image.js
@@ -12,7 +12,7 @@ export default function CoverImage({ title, url, slug }) {
     />
   )
   return (
-    <div className="sm:mx-5">
+    <div className="sm:mx-0">
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a aria-label={title}>{image}</a>
