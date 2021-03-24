ec00845b44faf314018031529cf68eebf06229d3
Gabriel Luiz <gabeluiz10@gmail.com>
2021-03-24 20:16:16 -0300

    update layout

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ export default class MyDocument extends Document {
         <Head>
         <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+        {/*ad*/}
+        <script data-ad-client="ca-pub-5573529617308034" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
