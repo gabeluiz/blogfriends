11da5949dc6ef32e718344a5d168115bf81bea27
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-03-29 17:19:20 -0300

    BuyMeaCoffee to other pages
    - Add BuyMeaCoffee to other pages
    - cannot fix the icon problem

--- components/layout.js
+++ components/layout.js
@@ -10,6 +10,14 @@ export default function Layout({ preview, children }) {
         <NavBar preview={preview} />
         <main className="mt-28">{children}</main>
       </div>
+      {/* PRA AJUDAR NOIS COM AS CONTA VAI QUE ALGUEM AJUDA HAHHAHAHAHHAH*/}
+      <div className=" hidden sm:block flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+        <div>
+          <a title="Buy me a coffe" href="/buymeacoffe" target="_blank" className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+            <img className="object-cover object-center w-full h-full rounded-full" src="images/coffe.png" />
+          </a>
+        </div>
+      </div>
       <Footer />
     </>
   )
